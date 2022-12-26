8812389de787837a51b676edeb05744ea180d4c3
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-26 21:23:21 +0100

    2fa field

--- src/components/basicComponents/TwoFa/TwoFa.interface.ts
+++ src/components/basicComponents/TwoFa/TwoFa.interface.ts
@@ -1,3 +1,3 @@
 export interface TwoFaProps {
-
+  title: string;
 }
--- src/styles/basicComponents/TwoFa.style.ts
+++ src/styles/basicComponents/TwoFa.style.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const InputTwoFa = styled.input`
+  margin: 4px;
+  height: 4rem;
+  width: 4rem;
+  font-family: "Lato", sans-serif;
+  box-sizing: border-box;
+  line-height: 24px;
+  transition: .3s;
+  font-size: 32px;
+  font-weight: 900;
+  outline: none;
+  border-radius: 6px;
+  caret-color: transparent;
+  border: 1px solid rgba(${(props) => props.theme.colors.primary}, .3);
+  text-align: center;
+  color: white;
+  background-color: rgba(35, 36, 40, 0.85);
+  &:focus {
+    outline: none !important;
+    border: 1px solid rgba(${(props) => props.theme.colors.primary}, 1);
+  }
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    display: none;
+  }
+`;
+
+export const Placeholder = styled.p`
+  opacity: .9;
+  font-size: 16px;
+  margin: 15px 5px;
+  font-weight: 200;
+`;
+
