5215235ee63da07eb67adbeeec35c45f3bf8c1dc
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-01-29 14:32:37 +0100

    settings page

--- src/pages/[locale]/account/settings.tsx
+++ src/pages/[locale]/account/settings.tsx
@@ -1,7 +1,41 @@
-const AccountSettings = () => {
+import {useTranslation} from "next-i18next";
+import {useRouter} from "next/router";
+import { getStaticPaths, makeStaticProps } from '@lib/getStatic';
+import React from "react";
+import Head from "next/head";
+import DefaultLayout from "@layouts/Default.layout";
+
+interface AccountSettingsProps {
+  locale: string;
+}
+
+const AccountSettings = ({ locale }: AccountSettingsProps) => {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const token = sessionStorage.getItem('_at');
+
+    if (!token) handleRedirect('/').then();
+  }, []);
+
+  const handleRedirect = async (path: string) => {
+    await router.push(`/${locale}${path}`);
+  };
+
   return (
-    <></>
+    <>
+      <Head>
+        <title>Cryptodistrict | {t('pages:account.settingsTitle')}</title>
+      </Head>
+      <DefaultLayout locale={locale} translate={t}>
+        <></>
+      </DefaultLayout>
+    </>
   );
 };
+
+const getStaticProps = makeStaticProps(['pages', 'components', 'errors', 'placeholders']);
+export { getStaticPaths, getStaticProps };
 
 export default AccountSettings;
