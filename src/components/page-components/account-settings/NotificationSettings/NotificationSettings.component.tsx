13b6411ed3e75566808ecf8d8d01232e85291d1a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-02-02 20:32:55 +0100

    notification pages

--- src/components/page-components/account-settings/NotificationSettings/NotificationSettings.component.tsx
+++ src/components/page-components/account-settings/NotificationSettings/NotificationSettings.component.tsx
@@ -1,14 +1,40 @@
 import {
   NotificationSettingsProps
 } from '@components/account-settings/NotificationSettings/NotificationSettings.interface';
-import { ItemContainer } from '@styles/NotificationSettings.style';
+import { Checkbox } from '@components/Checkbox/Checkbox.component';
+import {
+  ItemDescription,
+  ItemTitle,
+  Line,
+  NotificationItemBlock,
+  NotificationItemWrapper,
+  NotificationTitle,
+  NotificationTitleBox
+} from '@styles/NotificationSettings.style';
 
 const NotificationSettings = ({ locale, translate, notificationSettings }: NotificationSettingsProps) => {
   return (
     <>
-      <ItemContainer>
+      <NotificationTitleBox>
+        <NotificationTitle>
+          {translate('placeholders:inputs.notificationSettings')}
+        </NotificationTitle>
+        <Line />
+      </NotificationTitleBox>
 
-      </ItemContainer>
+      <NotificationItemBlock>
+        <NotificationItemWrapper>
+          <ItemTitle>Receive notifications</ItemTitle>
+          <ItemDescription>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, magni?</ItemDescription>
+        </NotificationItemWrapper>
+        <NotificationItemWrapper>
+          <Checkbox
+            value={notificationSettings?.receiveNotifications}
+            label={''}
+            onChange={() => {}}
+          />
+        </NotificationItemWrapper>
+      </NotificationItemBlock>
     </>
   );
 };
--- src/styles/page-components/NotificationSettings.style.ts
+++ src/styles/page-components/NotificationSettings.style.ts
@@ -1,14 +1,40 @@
 import styled from 'styled-components';
 
-export const ItemContainer = styled.div`
+export const NotificationTitleBox = styled.div`
+  margin-bottom: 50px;
+`;
+
+export const NotificationTitle = styled.h1`
+  padding-bottom: 10px;
+  color: rgb(${(props) => props.theme.colors.textColor});
+`;
+
+export const Line = styled.hr`
+  opacity: .5;
+  color: rgb(${(props) => props.theme.colors.opposite});
+`;
+
+export const NotificationItemBlock = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
+  height: 100px;
+  align-items: center;
   border-bottom: 1px solid rgba(${(props) => props.theme.colors.opposite}, .25);
-  padding-bottom: 10px;
-  margin: 5px 0;
 `;
 
-export const ItemHeader = styled.h3``;
+export const NotificationItemWrapper = styled.div`
+  &.button {
+    width: 200px;
+  }
+`;
 
-export const ItemDescription = styled.p``;
+export const ItemTitle = styled.h2`
+  color: rgb(${(props) => props.theme.colors.textColor});
+`;
+
+export const ItemDescription = styled.p`
+  color: rgb(${(props) => props.theme.colors.textColor});
+  opacity: .5;
+`;
+
